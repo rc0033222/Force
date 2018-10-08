0efc8b055c6c91093cecec01c6f766142d78d2f5
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-10-08 15:53:28 +0100

    resized my window

--- Force/Form1.Designer.cs
+++ Force/Form1.Designer.cs
@@ -113,7 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(450, 422);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
